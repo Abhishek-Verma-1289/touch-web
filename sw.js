d990a286b8b86c9507633b8f22e8f4224f0f0be4
Abhishek-Verma-1289 <vermaabhi1289@gmail.com>
2025-09-19 02:22:12 +0530

    Fix PWA paths and icons

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = 'lpu-touch-v1';
+const CACHE_NAME = 'lpu-touch-v2';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/dashboard.html',
-    '/mess.html',
-    '/scanner.html',
-    '/approve.html',
-    '/breakfast.html',
-    '/lunch.html',
-    '/tea.html',
-    '/dinner.html',
-    '/dashboard.css',
-    '/manifest.json',
-    '/assests/nav.jpg',
-    '/assests/foot.jpg',
-    '/assests/dashboard.jpg',
-    '/assests/mess.jpg',
-    '/assests/mess3.jpg',
-    '/assests/approvedi.jpg',
-    '/assests/approvedf.jpg',
-    '/assests/logo192.jpg',
-    '/assests/logo512.jpg'
+    './',
+    './index.html',
+    './dashboard.html',
+    './mess.html',
+    './scanner.html',
+    './approve.html',
+    './breakfast.html',
+    './lunch.html',
+    './tea.html',
+    './dinner.html',
+    './dashboard.css',
+    './manifest.json',
+    './assests/nav.jpg',
+    './assests/foot.jpg',
+    './assests/dashboard.jpg',
+    './assests/mess.jpg',
+    './assests/mess3.jpg',
+    './assests/approvedi.jpg',
+    './assests/approvedf.jpg',
+    './assests/logo192.jpg',
+    './assests/logo512.jpg'
 ];
 
 self.addEventListener('install', (event) => {
